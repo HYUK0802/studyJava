7281e7322970ae2d5757c576a1334b81d185ec54
hyuk <tig05205@naver.com>
2024-06-01 00:45:19 +0900

    study

--- src/Chapter3/ch02/Customer.java
+++ src/Chapter3/ch02/Customer.java
@@ -2,14 +2,11 @@ package Chapter3.ch02;
 
 public class Customer {
 
-    private int customerID;
-    private String customerName;
-    private String customerGrade;
+    protected int customerID;
+    protected String customerName;
+    protected String customerGrade;
     int bonusPoint;
     double bonusRatio;
-
-    String agentID;
-    double saleRatio;
 
     public Customer() {
 
@@ -17,14 +14,56 @@ public class Customer {
         bonusRatio = 0.01;
     }
     public int calcPrice(int price) {
-        if(customerGrade == "SILVER") {
-            bonusPoint += price * bonusRatio;
-        }else if(customerGrade == "VIP") {
-
-        }
-       return price;
+        bonusPoint += price * bonusRatio;
+        return price;
     }
+
+    public int getCustomerID() {
+        return customerID;
+    }
+
+    public Customer setCustomerID(int customerID) {
+        this.customerID = customerID;
+        return this;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public Customer setCustomerName(String customerName) {
+        this.customerName = customerName;
+        return this;
+    }
+
+    public String getCustomerGrade() {
+        return customerGrade;
+    }
+
+    public Customer setCustomerGrade(String customerGrade) {
+        this.customerGrade = customerGrade;
+        return this;
+    }
+
+    public int getBonusPoint() {
+        return bonusPoint;
+    }
+
+    public Customer setBonusPoint(int bonusPoint) {
+        this.bonusPoint = bonusPoint;
+        return this;
+    }
+
+    public double getBonusRatio() {
+        return bonusRatio;
+    }
+
+    public Customer setBonusRatio(double bonusRatio) {
+        this.bonusRatio = bonusRatio;
+        return this;
+    }
+
     public String showCustomerInfo() {
-        return customerGrade + "님의 등급은 " + customerGrade + "이며, 보너스 포인트는 " + bonusPoint + "입니다.";
+        return customerName + "님의 등급은 " + customerGrade + "이며, 보너스 포인트는 " + bonusPoint + "입니다.";
     }
 }
---
+++ src/Chapter3/ch06/VIPCustomer.java
@@ -0,0 +1,39 @@
+package Chapter3.ch06;
+
+public class VIPCustomer extends Customer {
+
+
+
+    private int agentID;
+    double salesRatio;
+
+	/*public VIPCustomer() {
+		customerGrade = "VIP";    //오류 발생
+		bonusRatio = 0.05;
+		salesRatio = 0.1;
+
+		System.out.println("VIPCustomer() 생성자 호출");
+	}
+	*/
+
+    public VIPCustomer(int customerID, String customerName) {
+        super(customerID, customerName);
+
+        customerGrade = "VIP";
+        bonusRatio = 0.05;
+        salesRatio = 0.1;
+
+
+    }
+
+    @Override
+    public int calcPrice(int price) {
+        bonusPoint += price * bonusRatio;
+        price -= (int)(price * salesRatio);
+        return price;
+    }
+
+    public int getAgentID() {
+        return agentID;
+    }
+}
