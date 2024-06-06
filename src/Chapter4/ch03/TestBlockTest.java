d45c5defb660b2bf4e06e4aa6c724bd3ce8f99a2
hyuk <tig05205@naver.com>
2024-06-06 10:38:36 +0900

    study

---
+++ src/Chapter4/ch03/TestBlockTest.java
@@ -0,0 +1,13 @@
+package Chapter4.ch03;
+
+public class TestBlockTest {
+    public static void main(String[] args) {
+
+        String textBlocks = """
+                Hello,
+                hi,
+                how r u""";
+        System.out.println(textBlocks);
+
+    }
+}
